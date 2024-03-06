39a2a0401509cc524490cda917a5877e6ef23987
Aritz4 <aritzh@gmail.com>
2024-03-06 10:02:53 +0100

    Segunda Validacion Clase Principal En rama

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,5 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("segunda Validacion");
+		System.out.println("Adios");
+		System.out.println("Pruebads Git.Se añade Cambio1");
 	}
 }
